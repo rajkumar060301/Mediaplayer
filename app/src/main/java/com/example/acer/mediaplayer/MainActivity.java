720e61d2cc948f463423b8f2aef939fa32689dd0
Rajkumar6301 <raj.kumar_cs19@gla.ac.in>
2021-07-01 23:09:27 +0530

    Initial commit

---
+++ app/src/main/java/com/example/acer/mediaplayer/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.acer.mediaplayer;
+
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button b1,b2;
+    MediaPlayer m1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        b1=(Button)findViewById(R.id.button);
+        b2=(Button) findViewById(R.id.button2);
+        m1=MediaPlayer.create(this,R.raw.s);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                m1.start();
+            }
+        });
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                m1.pause();
+            }
+        });
+    }
+}
